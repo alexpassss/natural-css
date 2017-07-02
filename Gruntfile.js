fcdc3827514e3e321a8af5f3755e3413bd86ec01
alexpassss <alexpassss@gmail.com>
2017-07-02 21:22:59 +0300

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,78 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    sass: {
+      dist: {
+          files: [
+              {outputstyle: '', expand: true, cwd: 'src/1_root', src: ['root.scss'], dest: 'dist/modules/1_root', ext: '.css'},
+              {outputstyle: '', expand: true, cwd: 'src/3_branches', src: ['branches.scss'], dest: 'dist/modules/3_branches', ext: '.css'},
+            //   {outputstyle: '', expand: true, cwd: 'src/7_characteristics_helpers', src: ['characteristics_helpers.scss'], dest: 'dist/modules/7_characteristics_helpers', ext: '.css'},
+              {outputstyle: '', expand: true, cwd: 'src/7_characteristics_helpers', src: ['box_offset_helpers.scss'], dest: 'dist/modules/7_characteristics_helpers', ext: '.css'},
+              {outputstyle: '', expand: true, cwd: 'src/7_characteristics_helpers', src: ['contextual_backgrounds_helpers.scss'], dest: 'dist/modules/7_characteristics_helpers', ext: '.css'},
+              {outputstyle: '', expand: true, cwd: 'src/7_characteristics_helpers', src: ['contextual_colors_helpers.scss'], dest: 'dist/modules/7_characteristics_helpers', ext: '.css'},
+              {outputstyle: '', expand: true, cwd: 'src/7_characteristics_helpers', src: ['font_size_helpers.scss'], dest: 'dist/modules/7_characteristics_helpers', ext: '.css'},
+              {outputstyle: '', expand: true, cwd: 'src/7_characteristics_helpers', src: ['line_height_helpers.scss'], dest: 'dist/modules/7_characteristics_helpers', ext: '.css'},
+              {outputstyle: '', expand: true, cwd: 'src/7_characteristics_helpers', src: ['text_helpers.scss'], dest: 'dist/modules/7_characteristics_helpers', ext: '.css'},
+              {outputstyle: '', expand: true, cwd: 'src/5_elements', src: ['elements.scss'], dest: 'dist/modules/5_elements', ext: '.css'}
+          ]
+      }
+    },
+
+    // concat: {
+    //   apps: {
+    //       src: 'lib/modules/*.css',
+    //       dest: 'lib/dist/tinycsshelpers.css'
+    //   }
+    // },
+    //
+    // cssmin: {
+    //   minify: {
+    //     expand: true,
+    //     cwd: 'lib/dist',
+    //     src: ['*.css', '!*.min.css'],
+    //     dest: 'lib/dist',
+    //     ext: '.min.css'
+    //   },
+    //   release: {
+    //     expand: true,
+    //     cwd: 'lib/dist',
+    //     src: ['*.css', '!*.min.css'],
+    //     dest: 'lib/dist',
+    //     ext: '.min.css'
+    //   }
+    // },
+    //
+    // copy: {
+    //     main: {
+    //         files: [
+    //           {expand: true, cwd: 'sources/fonts/', src: ['**'], dest: 'lib/dist/fonts'}
+    //         ],
+    //     },
+    // },
+
+    watch: {
+      sass: {
+          files: '**/*.scss',
+          tasks: ['sass'],
+      },
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-sass');
+  // grunt.loadNpmTasks('grunt-contrib-concat');
+  // grunt.loadNpmTasks('grunt-contrib-cssmin');
+  // grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task.
+  grunt.registerTask('default', [
+    'sass'
+    // 'concat',
+    // 'cssmin:minify',
+    // 'cssmin:release',
+    // 'copy'
+  ]);
+  grunt.registerTask('reload', ['watch']);
+
+};
